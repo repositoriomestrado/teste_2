7b3828fa5f2a4d4eece14ef1d6c956694c78de35
thiago <thiago@thiago.thiago>
2018-11-27 20:58:59 -0200

    teste

--- src/main/java/br/com/wtsyst/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.wtsyst.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = { "br.com.wtsyst" })
-public class AppConfig {
-}
